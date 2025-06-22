881f380bd71beaaf36604cd6c96936ca86140871
Buicongbang04 <buicongbang1010@gmail.com>
2025-06-22 11:47:52 +0700

    first commit

---
+++ etl/etl_script.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+    retries = 0
+    while retries < max_retries:
+        try:
+            result = subprocess.run(
+                ["pg_isready", "-h", host],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            if "accepting connections" in result.stdout:
+                print(f"PostgreSQL is ready at {host}.")
+                return True
+        except subprocess.CalledProcessError as e:
+            print(f"PostgreSQL is not ready yet: {e}. Retrying...")
+            retries += 1
+            print(f"Retrying in {delay_seconds} seconds... (Attempt {retries}/{max_retries})")
+            time.sleep(delay_seconds)
+    print("Max retries reached. PostgreSQL is not ready.")  
+    return False 
